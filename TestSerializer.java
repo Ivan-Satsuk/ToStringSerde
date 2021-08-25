cd0b812da9c4b25fef6e0f6ec68459161c1646c8
Ivan-Satsuk <iwnian@mail.ru>
2021-08-25 22:40:22 +0500

    format

--- TestSerializer.java
+++ TestSerializer.java
@@ -1,7 +1,12 @@
 package CountSort;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TestSerializer {
     public static void main(String[] args) {
@@ -15,10 +20,18 @@ public class TestSerializer {
         for (int i = 0; i < 100; i++) {
             Random random = new Random();
             int array_length = random.nextInt(200) + 1;
-            short[] shortsBeforeSer = new short[array_length];
 
-            for (int j = 0; j < shortsBeforeSer.length; j++) {
-                shortsBeforeSer[j] = (short) (random.nextInt(999) + 1);
+            short[] shortsBeforeSer = new short[array_length];
+            HashSet<Short> shorts = new HashSet<>();
+            int chek = 0;
+
+            while (chek < array_length) {
+                short nextShort = (short) (random.nextInt(999)+1);
+                if (!shorts.contains(nextShort)){
+                    shorts.add(nextShort);
+                    shortsBeforeSer[chek]=nextShort;
+                    chek+=1;
+                }
             }
 
             String s = ToStringSerializer.serializeShortsToString(shortsBeforeSer);
@@ -27,6 +40,7 @@ public class TestSerializer {
         }
 
         System.out.printf("Все 100 случаев массивы до сериализации и после равны - %b\n", Arrays.equals(expectedBooleans, result));
+
 
 
 //        // Test with array capacity = 1
@@ -39,11 +53,21 @@ public class TestSerializer {
         System.out.printf("Массивы до сериализации и после равны - %b\n", Arrays.equals(shortsBeforeSer, shortsAfterDes));
 
 
+
 //        test with max array capacity = 200
         System.out.print("Test 3: \n");
+        
         short[] shortsBeforeSerTest3 = new short[200];
-        for (int j = 0; j < shortsBeforeSerTest3.length; j++) {
-            shortsBeforeSerTest3[j] = (short) (random.nextInt(999) + 1);
+        HashSet<Short> shorts = new HashSet<>();
+        int chek = 0;
+
+        while (chek < 200) {
+            short nextShort = (short) (random.nextInt(999)+1);
+            if (!shorts.contains(nextShort)){
+                shorts.add(nextShort);
+                shortsBeforeSerTest3[chek]=nextShort;
+                chek+=1;
+            }
         }
         String charsResultTest3 = ToStringSerializer.serializeShortsToString(shortsBeforeSerTest3);
 
